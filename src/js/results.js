c129f143c0c810e0cdf000e51144d52bb03e1b33
IdrisDW <33130924+IdrisDW@users.noreply.github.com>
2021-08-18 12:46:33 -0700

    Updated results : browse by name functionality in conjunction with Recipe page

--- src/js/results.js
+++ src/js/results.js
@@ -9,45 +9,51 @@ const resultsTitle = document.getElementById("resultsTitle");
 const meals = document.getElementById("meals");
 const dish = document.getElementById("dish");
 const inputSearch = localStorage.getItem('inputSearch');
-
-//one or more results
+ 
+ 
+// this function lets us get the results from the input text and display it on a grid
 function lookUp(event) {
   event.preventDefault();
   dish.innerHTML = "";
   const searchValue = search.value;
-  console.log(searchValue)
   //    alert(searchValue);
   const isEmpty = (str) => !searchValue.trim().length;
+  // we check if nothing has been written
   if (isEmpty(this.value)) {
     alert("Please enter the name of a meal");
   } else {
+    // we get the data from the API
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
         console.log(data);
+        // we display it on the grid
         resultsTitle.innerHTML = `
         <h2> Results for ${searchValue} : </h2>`;
+
         if (data.meals === null) {
-          resultsTitle.innerHTML = 
           `<h2>Sorry, there are no recipes to show</h2>`;
         } else {
           meals.innerHTML = data.meals
             .map(function (meal) {
-              return `   
+              return `
+                      
                         <div class="dish">
+
                         <a href="./recipe.html" id="selected" value="${meal.idMeal}" >${meal.strMeal}</a>
-                          <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                        </div>
-                      `;
-            })
-            .join("");
-        }
-      });
-  }
-}
-searchButton.addEventListener('click', lookUp);
+                                                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                                                </div>
+                                              `;
+                                   })
+                                   .join("");
+                                 }
+                              });
+                           }
+                         }
+                         searchButton.addEventListener('click', lookUp);
+                        
 
 //random search
 function Random(event) {
@@ -77,130 +83,70 @@ function Random(event) {
 random.addEventListener('click', Random);
 
 
+
+//from here Browse by name
+
 const dLetter = document.querySelector(".containerBrowseByName");
-dLetter.addEventListener("click", lookUpAlpha);
 
-function lookUpAlpha(event) {
+//browse by name bar is generated
+let alpha = Array.from(Array(26)).map((e, i) => i + 65);
+let alphabet = alpha.map((x) => String.fromCharCode(x));
 
-    // alert(event.target.getAttribute('id'));
-
-alert(event.currentTarget.value());//()
-    // event.target.value());
-    // const bLetter = search.value;
-    // fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
-    //       .then(function (response) {
-    //         return response.json();
-    //       })
-    //       .then(function (data) {
-    //         console.log(data);
-    //         resultsTitle.innerHTML = `<h2> Results for ${searchValue} : </h2>`;
-    
-    //         if (data.meals === null) {
-    //           `<h2>Sorry, there are no recipes to show</h2>`;
-    //         } else {
-    //           meals.innerHTML = data.meals
-    //             .map(function (meal) {
-    //               return `
-                            
-    //                           <div class="dish">
-    //                           <h2>${meal.strMeal}</h2>
-    //                             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                           
-    //                           </div>
-    //                         `;
-    //             })
-    //             .join("");
-    //         }
-    //       });
-
-
-
-    // alert("hola");
-//   event.preventDefault();
-//   dish.innerHTML = "";
-//   const searchValue = search.value;
-//   //    alert(searchValue);
-//   const isEmpty = (str) => !searchValue.trim().length;
-//   if (isEmpty(this.value)) {
-//     alert("Please enter the name of a meal");
-//   } else {
-//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data);
-//         resultsTitle.innerHTML = `<h2> Results for ${searchValue} : </h2>`;
-
-//         if (data.meals === null) {
-//           `<h2>Sorry, there are no recipes to show</h2>`;
-//         } else {
-//           meals.innerHTML = data.meals
-//             .map(function (meal) {
-//               return `
-                        
-//                           <div class="dish">
-//                           <h2>${meal.strMeal}</h2>
-//                             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                       
-//                           </div>
-//                         `;
-//             })
-//             .join("");
-//         }
-//       });
-//   }
+let html = "";
+//loop through the alphabet to generate the buttons
+for (let x = 0; x < alphabet.length; x++) {
+  html += `
+    <button type="submit" 
+    class="containerBrowseByName browseLetter"
+    value="${alphabet[x]}"
+    id="${alphabet[x]}">
+        <h6 browseLetterS="${alphabet[x]}">
+        ${alphabet[x]}</h6>
+    </button>
+    `;
 }
- 
 
-// // trying to join them
+// we add it to the html
 
-// function lookUpAlpha3(event) {
+dLetter.innerHTML = html;
 
-// // alert(event.target.dataset.id)
-//     // let brLetter = event.target.dataset.id;
-//     alert(event.target.getAttribute('id'));
-//     alert(event.target.value);
-//     // alert(brLetter);
+// set event listener to the buttons, first by selecting the classes
+let Buttons = document.querySelectorAll(".containerBrowseByName  button ");
 
-// }
+// loop through the buttons using for..of
+for (let button of Buttons) {
+  // listen for a click event
+  button.addEventListener("click", (e) => {
+    // store the current clicked button
+    let brLetter = e.currentTarget.value;
 
-// function lookUpAlpha(event) {
+    //f r o m h e r e
+    // we get the results according to whatever value of the button has been clicked
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${brLetter}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        let html = "";
 
-// //   event.preventDefault();
+        if (data.meals === null) {
+          `<h2>Sorry, there are no recipes to show</h2>`;
+        } else {
+          meals.innerHTML = data.meals
+          .map(function (meal) {
+            return `
+                    
+                      <div class="dish">
 
-// //   let brLetter = event.target.dataset.id; //kinda
-// // alert(event.target.getAttribute('id'));
-//   // dish.innerHTML= '';
-//   // const searchValue = search.value;
-//   //    alert(searchValue);
-
-// //   let brLetter = event.target.getAttribute('id');
-// //   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`)
-//   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${brLetter}`)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       let html = "";
-
-//       if (data.meals === null) {
-//         `<h2>Sorry, there are no recipes to show</h2>`;
-//       } else {
-
-//         meals.innerHTML = data.meals
-//           .map(function (meal) {
-//             return `
-
-//                         <div class="dish">
-//                         <h2>${meal.strMeal}</h2>
-//                           <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-
-//                         </div>
-//                       `;
-//           })
-//           .join("");
-//       }
-//     });
-// }
+                      <a href="./recipe.html" id="selected" value="${meal.idMeal}" >${meal.strMeal}</a>
+                                                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                                              </div>
+                                            `;
+                                 })
+                                 .join("");
+                               }
+     
+      });
+  });
+}
